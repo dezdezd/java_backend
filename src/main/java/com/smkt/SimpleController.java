0667a4bae7a9476430b4c15f2a3ed43e8b00f93c
Вадим Фролов <vadimfrolov@MacBook-Pro-Vadim.local>
2024-06-01 17:54:19 +0300

    Initial commit

---
+++ src/main/java/com/smkt/SimpleController.java
@@ -0,0 +1,15 @@
+package com.smkt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
