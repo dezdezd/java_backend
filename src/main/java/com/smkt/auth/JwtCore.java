9bf90909c936d5d53e52647552791150e47ff50f
vadimfrolov <vadim.frolov.2018@inbox.ru>
2024-07-05 19:02:30 +0300

    fix JWT

--- src/main/java/com/smkt/auth/JwtCore.java
+++ src/main/java/com/smkt/auth/JwtCore.java
@@ -1,8 +1,13 @@
 package com.smkt.auth;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtCore {
@@ -12,6 +17,14 @@ public class JwtCore {
     private Integer lifetime;
 
     public String generateToken(Authentication authentication) {
-        return "";
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + lifetime))
+                .signWith(key)
+                .compact();
     }
 }
